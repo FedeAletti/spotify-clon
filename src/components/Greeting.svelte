c544253e486918fc3bd745b71ec065a1a7119a32
Federico Aletti <72565553+FedeAletti@users.noreply.github.com>
2023-10-07 21:14:43 -0300

    Primera version

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+
+    let greeting = ""
+
+    if (hours < 12) {
+        greeting = "Good Morning";
+    } else if (hours < 18) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good Evening";
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
